7166b6ef82056c36a2fbe5aa96f9da0ea80efabc
yaelrosen77 <yaelrosen10@gmail.com>
2023-04-09 20:32:34 +0300

    #2 commit

--- sources/player.cpp
+++ sources/player.cpp
@@ -1,1 +1,20 @@
 #include "player.hpp"
+
+Player :: Player(string nombre){
+    name = nombre;
+    mazoCartas = new Stack(HALF_DECK);
+    loot = new Stack(CAPACITY);
+}
+
+Player ::~Player(){
+    delete mazoCartas;
+    delete loot;
+}
+
+unsigned int Player :: stacksize(){
+    return mazoCartas->CurSize;
+} 
+
+unsigned int Player:: cardesTaken(){
+    return loot->CurSize;
+}
--- sources/player.hpp
+++ sources/player.hpp
@@ -3,15 +3,22 @@
 #include "stack.hpp"
 #include <iostream>
 #include <cstdlib>
-#define 
+#define HALF_DECK 26
+#define CAPACITY 52
+
 using namespace std;
 
 class Player{
-    Stack mazoCartas(26);
+    Stack * mazoCartas;
     
     public: 
         string name;
-        Stack loot;
+        Stack * loot;
+
+        Player(string nombre);
+        ~Player();
+        unsigned int stacksize();
+        unsigned int cardesTaken();
 };
 
 #endif
